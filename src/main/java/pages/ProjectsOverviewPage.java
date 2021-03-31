40d032c52c02b7f30aa7d2e49e9b57e4733f51da
Raji Shetti <rajalaxmishetti@gmail.com>
2021-03-31 11:35:57 -0700

    An automation framework using Java, Selenium Webdriver, TestNG Framework and POM Design Patternv2

--- src/main/java/pages/ProjectsOverviewPage.java
+++ src/main/java/pages/ProjectsOverviewPage.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.PageFactory;
 
 public class ProjectsOverviewPage extends BasePage{
 
+    //using FindBy library
     @FindBy(id = "navigation-dashboard")
     WebElement dashboardLink;
 
